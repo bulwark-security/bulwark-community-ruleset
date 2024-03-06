a9ce634a8eb504754bb77970a35a00bcc76bc0db
maliz-bulwark <maliz@bulwark.security>
2024-03-05 16:19:18 -0800

    address comments

--- rules/numeric-host/src/lib.rs
+++ rules/numeric-host/src/lib.rs
@@ -1,6 +1,7 @@
 use anyhow::{anyhow, Ok};
 use bulwark_wasm_sdk::*;
-use regex::*;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 struct NumericHostPlugin;
 
@@ -10,44 +11,18 @@ fn check_numeric_host(req: Request) -> std::result::Result<bool, anyhow::Error> 
         .get("Host")
         .ok_or(anyhow!("Missing Host Header."))?;
 
-    let re_ipv4 = Regex::new(r"^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}(:[0-9]{1,5})?$").unwrap();
-    let re_ipv6 = Regex::new(r"\[(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}\](:[0-9]{1,5})?|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))").unwrap();
-    // numerals as IPv4
-    let re_decimal = Regex::new(r"^[0-9]{1,10}(:[0-9]{1,5})?$").unwrap();
-    let re_binary = Regex::new(r"^0[bB][0-1]{1,32}(:[0-9]{1,5})?$").unwrap();
-    let re_hex = Regex::new(r"^0[xX][0-9a-fA-F]{1,8}(:[0-9]{1,5})?$").unwrap();
-    let re_octal = Regex::new(r"^0[0-7]{1,11}(:[0-9]{1,5})?$").unwrap();
-
-    let match_ipv4 = re_ipv4.find(host.to_str()?);
-    let match_ipv6 = re_ipv6.find(host.to_str()?);
-    let match_decimal = re_decimal.find(host.to_str()?);
-    let match_binary = re_binary.find(host.to_str()?);
-    let match_hex = re_hex.find(host.to_str()?);
-    let match_octal = re_octal.find(host.to_str()?);
-
-    match (
-        match_ipv4,
-        match_ipv6,
-        match_decimal,
-        match_binary,
-        match_hex,
-        match_octal,
-    ) {
-        (None, None, None, None, None, None) => Ok(false),
-        _ => Ok(true),
+    lazy_static! {
+        static ref NUMERIC_HOST_REGEX: Regex =
+        Regex::new(r"^((?:(?:[0-9]+|0(x|X)[0-9a-fA-F]+|0(b|B)[0-7]+)\.){0,3}(?:[0-9]+|0(x|X)[0-9a-fA-F]+|0(b|B)[0-7]+)|\[[0-9a-fA-F:]+\])(:[0-9]{1,5})?$")
+            .unwrap();
     }
+    let match_invalid_host = NUMERIC_HOST_REGEX.find(host.to_str()?);
+    Ok(match_invalid_host.is_some())
 }
 
 #[bulwark_plugin]
 impl Handlers for NumericHostPlugin {
     fn on_request_decision() -> Result {
-        if check_numeric_host(get_request())? {
-            set_restricted(1.0);
-        }
-        Ok(())
-    }
-
-    fn on_request_body_decision() -> Result {
         if check_numeric_host(get_request())? {
             set_restricted(1.0);
         }
@@ -64,6 +39,10 @@ mod tests {
         let test_cases = [
             ("www.bulwark.security", false),
             ("www.bulwark.security:80", false),
+            ("www.bul-wark.security:80", false),
+            ("www.bul-wark.123", false),
+            ("www.bul-wark98987.security:80", false),
+            ("0xwww.bul-wark98987.security:80", false),
             // IPv4
             ("127.0.0.1", true),
             ("127.0.0.1:80", true),
@@ -71,11 +50,10 @@ mod tests {
             ("192.168.1.255", true),
             ("255.255.255.255", true),
             ("0.0.0.0", true),
-            ("3...3", false),
-            ("30.168.1.255.1", false),
-            ("127.1", false),
-            ("192.168.1.256", false),
-            ("-1.2.3.4", false),
+            ("127.1", true),
+            ("192.168.1.256", true),
+            ("0x8.0X8.010.8", true),
+            ("0x1.0x1.0x1.0x1", true),
             // IPv6
             ("[1:2:3:4:5:6:7:8]", true),
             ("[1:2:3:4:5:6:7:8]:80", true),
@@ -101,14 +79,11 @@ mod tests {
             ("[::8]", true),
             ("[::]", true),
             ("[::]:443", true),
-            ("[fe80::7:8%eth0]", true),
-            ("[fe80::7:8%1]", true),
             // numerals
             ("2147483647", true),
             ("0", true),
             ("909090:8080", true),
             ("0x989890fa", true),
-            ("0X989890fa", true),
             ("0x989890fa:8080", true),
             ("0b1010101010", true),
             ("0B1010101010", true),
